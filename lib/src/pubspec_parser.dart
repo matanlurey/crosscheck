a979c19862fd391bd4774e249cb592fb856d6206
Jonah Williams <jonahwilliams@users.noreply.github.com>
2017-07-21 13:46:03 -0700

    Add simple parser for pubspec (#2)

---
+++ lib/src/pubspec_parser.dart
@@ -0,0 +1,120 @@
+import 'package:yaml/yaml.dart';
+import 'package:pub_semver/pub_semver.dart';
+// import 'package:path/path.dart';
+
+/// The parsed contents of a pubspec file.
+///
+/// Shamelessly copied from https://github.com/dart-lang/pub/blob/master/lib/src/pubspec.dart.
+class Pubspec {
+  /// All pubspec fields.
+  final YamlMap fields;
+
+  String _name;
+  VersionConstraint _version;
+  List<PackageRange> _dependencies;
+  List<PackageRange> _devDependencies;
+
+  Pubspec(this.fields);
+
+  /// The package name.
+  ///
+  /// Throws [Exception] if the name is missing or invalid.
+  String get name {
+    if (_name != null) return _name;
+
+    final Object name = fields['name'];
+    if (name == null || name is! String) {
+      throw new Exception('Invalid packaged name: $name');
+    }
+    _name = name as String;
+    return _name;
+  }
+
+  /// The package version.
+  ///
+  /// Throws [Exception] if the version is missing or invalid.
+  VersionConstraint get version {
+    if (_version != null) return _version;
+
+    final Object version = fields['version'];
+    if (version == null) {
+      _version = Version.none;
+      return _version;
+    }
+    if (version is! String) {
+      throw new Exception('version must be a string');
+    }
+    _version = new VersionConstraint.parse(version as String);
+    return _version;
+  }
+
+  /// The package's dependencies.
+  List<PackageRange> get dependencies {
+    print(fields['dependencies']);
+    _dependencies ??=
+        _parseDependencies('dependencies', fields.nodes['dependencies']);
+    return _dependencies;
+  }
+
+  /// The package's dev dependencies.
+  List<PackageRange> get devDependencies {
+    _devDependencies ??= _parseDependencies(
+        'dev_dependencies', fields.nodes['dev_dependencies']);
+    return _devDependencies;
+  }
+
+  /// Parses the dependency field named [field], and returns the corresponding
+  /// list of dependencies.
+  List<PackageRange> _parseDependencies(String field, YamlNode node) {
+    final dependencies = <PackageRange>[];
+
+    /// Has no dependencies.
+    if (node == null || node.value == null) return dependencies;
+    final dependencyMap = node as YamlMap;
+    dependencyMap.nodes.forEach((YamlNode nameNode, YamlNode specNode) {
+      final name = nameNode.value as String;
+      final spec = specNode.value as Object;
+      VersionConstraint versionConstraint = new VersionRange();
+
+      if (spec is String) {
+        versionConstraint = _parseVersionConstraint(specNode);
+      } else if (spec is Map) {
+        final specMap = specNode as YamlMap;
+        final specCopy = new Map<String, YamlNode>.from(spec);
+
+        if (specCopy.containsKey('version')) {
+          specCopy.remove('version');
+          versionConstraint = _parseVersionConstraint(specMap.nodes['version']);
+        }
+      }
+      dependencies.add(new PackageRange(name, versionConstraint));
+    });
+    return dependencies;
+  }
+
+  VersionConstraint _parseVersionConstraint(YamlNode node,
+      {VersionConstraint defaultConstraint}) {
+    if (node?.value == null) return defaultConstraint;
+    if (node.value is! String) {
+      throw new Exception('A version constraint must be a  string');
+    }
+    return new VersionConstraint.parse(node.value as String);
+  }
+}
+
+/// A package name and constraint.
+class PackageRange {
+  final String name;
+  final VersionConstraint constraint;
+
+  PackageRange(this.name, this.constraint);
+
+  @override
+  bool operator ==(Object other) =>
+      other is PackageRange &&
+      name == other.name &&
+      !constraint.intersect(other.constraint).isEmpty;
+
+  @override
+  String toString() => '{name: $name, constraint: $constraint}';
+}
---
+++ test/pubspec_parser_test.dart
@@ -0,0 +1,50 @@
+import 'package:crosscheck/src/pubspec_parser.dart';
+import 'package:pub_semver/pub_semver.dart';
+import 'package:test/test.dart';
+import 'package:yaml/yaml.dart';
+
+const expectedMainVersion = '0.1.0';
+const expectedVersion = '^1.3.0';
+const testYamlFile = '''
+  name: crosscheck
+  description: >
+    Automated Dependency Management for Dart.
+  version: $expectedMainVersion
+  authors:
+    - Dart Team <misc@dartlang.org>
+  homepage: https://github.com/matanlurey/crosscheck
+
+  environment:
+    sdk: '>=1.24.0 <2.0.0-dev.infinity'
+
+  dependencies:
+    yaml: "$expectedVersion"
+    pub_semver: "$expectedVersion"
+    path: "$expectedVersion"
+
+  dev_dependencies:
+    test:
+
+  executables:
+    crosscheck:
+  ''';
+
+void main() {
+  group('Pubspec', () {
+    test('can parse a pubspec file and it\'s dependencies', () {
+      final yaml = loadYaml(testYamlFile) as YamlMap;
+      final pubspec = new Pubspec(yaml);
+      final expectedConstraint = new VersionConstraint.parse(expectedVersion);
+
+      expect(pubspec.name, 'crosscheck');
+      expect(
+          pubspec.dependencies,
+          unorderedEquals(<PackageRange>[
+            new PackageRange('yaml', expectedConstraint),
+            new PackageRange('pub_semver', expectedConstraint),
+            new PackageRange('path', expectedConstraint),
+          ]));
+      expect(pubspec.version, new VersionConstraint.parse(expectedMainVersion));
+    });
+  });
+}
