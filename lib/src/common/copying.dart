4a3036538eeb2e0af78d7048c52ac4aed6a393c3
Matan Lurey <matanlurey@users.noreply.github.com>
2017-07-21 14:58:29 -0700

    Add a copy utility and test (#6)
    * Add dartanalyzer-based check.
    
    * Add a copy utility and test.

---
+++ lib/src/common/copying.dart
@@ -0,0 +1,40 @@
+// Copyright 2017, Google Inc.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:path/path.dart' as p;
+
+/// Makes a copy of the folder at [path], and executes [run] with a new path.
+///
+/// May optionally specify the precise [destination] folder, otherwise it is
+/// provided automatically by the system.
+Future<T> withCopyOf<T>(
+  String path,
+  Future<T> run(String path), {
+  String destination,
+}) async {
+  destination ??= Directory.systemTemp.createTempSync().path;
+  final source = new Directory(path);
+  if (!source.existsSync()) {
+    throw new ArgumentError('No directory found at $path.');
+  }
+  await _copy(path, destination);
+  final result = await run(destination);
+  await new Directory(destination).delete(recursive: true);
+  return result;
+}
+
+Future<Null> _copy(String from, String to) async {
+  await new Directory(to).create(recursive: true);
+  await for (final file in new Directory(from).list(recursive: true)) {
+    final copyTo = p.join(to, p.relative(file.path, from: from));
+    if (file is Directory) {
+      await new Directory(copyTo).create(recursive: true);
+    } else if (file is File) {
+      await new File(file.path).copy(copyTo);
+    }
+  }
+}
---
+++ test/common/copying_test.dart
@@ -0,0 +1,27 @@
+// Copyright 2017, Google Inc.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:crosscheck/src/common/copying.dart';
+import 'package:path/path.dart' as p;
+import 'package:test/test.dart';
+
+void main() {
+  test('should make a deep copy of a folder', () async {
+    final destination = await withCopyOf(
+      p.join('package', 'has_no_error'),
+      (path) async {
+        expect(FileSystemEntity.isDirectorySync(path), isTrue);
+        expect(
+            FileSystemEntity.isFileSync(
+              p.join(path, 'lib', 'has_no_error.dart'),
+            ),
+            isTrue);
+        return path;
+      },
+    );
+    expect(FileSystemEntity.isDirectorySync(destination), isFalse);
+  });
+}
